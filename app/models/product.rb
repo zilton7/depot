c3d151fea7c82748e0df66fd961bbc48f2ee51a7
Zil Norvilis <zilasino27@gmail.com>
2021-10-05 08:20:35 +0200

    Add cart functionality

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,7 @@
 class Product < ApplicationRecord
+  has_many :line_items
+  before_destroy :ensure_not_referenced_by_any_line_item
+
   validates :title, :description, :image_url, presence: true
   validates :title, uniqueness: true
   validates :image_url, allow_blank: true, format: { 
@@ -6,4 +9,12 @@ class Product < ApplicationRecord
     message: 'must be a URL for GIF, JPG or PNG image'
   }
   validates :price, numericality: { greater_than_or_equal_to: 0.01 }
+
+  private
+  def ensure_not_referenced_by_any_line_item
+    unless line_items.empty?
+      errors.add(:base, 'Line items present')
+      throw :abort
+    end
+  end
 end
--- test/controllers/products_controller_test.rb
+++ test/controllers/products_controller_test.rb
@@ -38,6 +38,14 @@ class ProductsControllerTest < ActionDispatch::IntegrationTest
     patch product_url(@product), params: { product: { description: @product.description, image_url: @product.image_url, price: @product.price, title: @title } }
     assert_redirected_to product_url(@product)
   end
+  
+  test 'can\'t delete product in cart' do
+    assert_difference('Product.count', 0) do
+      delete product_url(products(:two))
+    end
+
+    assert_redirected_to products_url
+  end
 
   test "should destroy product" do
     assert_difference('Product.count', -1) do
@@ -46,4 +54,5 @@ class ProductsControllerTest < ActionDispatch::IntegrationTest
 
     assert_redirected_to products_url
   end
+
 end
