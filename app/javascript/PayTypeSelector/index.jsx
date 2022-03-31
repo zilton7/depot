94dd92f4e851c6a66bf2ba8a793b0935e2860e95
Zil Norvilis <zilasino27@gmail.com>
2022-03-31 18:19:19 +0300

    Update pay_type component

--- app/javascript/PayTypeSelector/index.jsx
+++ app/javascript/PayTypeSelector/index.jsx
@@ -1,16 +1,45 @@
 import React from 'react'
+import NoPayType from './NoPayType';
+import CheckPayType from "./CheckPayType";
+import CreditCardPayType from "./CreditCardPayType";
+import PurchaseOrderPayType from "./PurchaseOrderPayType"
 
 class PayTypeSelector extends React.Component {
-    render() { 
+    constructor(props) {
+      super(props);
+      this.onPayTypeSelected = this.onPayTypeSelected.bind(this);
+      this.state = { selectedPayType: null };
+    }
+    onPayTypeSelected(event){
+      this.setState({selectedPayType: event.target.value})
+      console.log(this.state.selectedPayType);
+    }
+
+    render() {
+      let PayTypeCustomComponent = NoPayType;
+      if (this.state.selectedPayType == 'Credit card'){
+        PayTypeCustomComponent = CreditCardPayType;
+      } else if (this.state.selectedPayType == 'Check'){
+        PayTypeCustomComponent = CheckPayType;
+      } else if (this.state.selectedPayType == 'Purchase order'){
+        PayTypeCustomComponent = PurchaseOrderPayType;
+      }
         return (
-          <div className="field">
-            <div htmlFor="order_pay_type">Pay Type</div>
-            <select id="order_pay_type" name="order[pay_type]">
-              <option value="">Select a payment method</option>
-              <option value="Check">Check</option>
-              <option value="Credit card">Credit card</option>
-              <option value="Purchase order">Purchase order</option>
-            </select>
+          <div>
+            <div className="field">
+              <label htmlFor="order_pay_type">Pay Type</label>
+              <select
+                id="order_pay_type"
+                onChange={this.onPayTypeSelected}
+                name="order[pay_type]"
+              >
+                <option value="">Select a payment method</option>
+                <option value="Check">Check</option>
+                <option value="Credit card">Credit card</option>
+                <option value="Purchase order">Purchase order</option>
+              </select>
+            </div>
+            <PayTypeCustomComponent/>
           </div>
         );
     }
